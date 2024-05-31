2f73ff17069e0d323fdee9f3c5931912897b2599
Patryk Mikuli <patryk.mikuli@gmail.com>
2024-05-31 18:47:23 +0200

    Rename duplicate error file

--- calendar.py
+++ calendar.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from datetime import timedelta
-from duplicateerror import DuplicateEventException
+from duplicate_error import DuplicateEventException
 from event import Event
 from typing import List
 
