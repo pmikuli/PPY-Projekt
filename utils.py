457443f8ba877640baef51224da66beb58a2dfaa
Patryk Mikuli <patryk.mikuli@gmail.com>
2024-05-31 16:40:08 +0200

    Functions to convert datetime from and to string, change how calendar is saved and loaded a bit

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 from typing import List
 from calendar import Calendar
+from datetime import datetime
 import event
 import csv
 
@@ -8,22 +9,44 @@ def save_calendar(calendar: Calendar, filename: str):
     with open(filename, 'w') as f:
         w = csv.writer(f)
         for e in calendar:
-            w.writerow(list(e))
-
+            f.write(date_to_str(e.date_from) + "," + date_to_str(e.date_to) + "," + e.name + "\n")
 
 def load_calendar(filename: str) -> Calendar:
     with open(filename, 'r') as f:
         events = []
         reader = csv.reader(f)
         for row in reader:
-            events.append(event.Event(row[0], row[1], row[2]))
+            date_from = parse_date_str(row[0])
+            date_to = parse_date_str(row[1])
+            events.append(event.Event(date_from, date_to, row[2]))
         calendar = Calendar(events)
         return calendar
-
-
-
 
 
 def print_list(lines: List):
     for i in lines:
         print(i)
+
+
+def parse_date_str(date_str: str) -> datetime:
+    split = date_str.split(" ")
+
+    d = split[0]
+
+    day = int(d.split("-")[0])
+    month = int(d.split("-")[1])
+    year = int(d.split("-")[2])
+
+    if len(split) == 2:
+        t = split[1]
+
+        hour = int(t.split(":")[0])
+        minutes = int(t.split(":")[1])
+        print(day)
+        return datetime(year, month, day, hour, minutes)
+    else:
+        return datetime(year, month, day)
+
+
+def date_to_str(date: datetime) -> str:
+    return date.strftime("%d-%m-%Y %H:%M")
